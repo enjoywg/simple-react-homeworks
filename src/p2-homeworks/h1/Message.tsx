0e72847b72eafec872126cdfab93c44f98848810
enjoyw <66209112+enjoywg@users.noreply.github.com>
2020-09-03 14:18:19 +0300

    hw1

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,30 @@
 import React from "react";
+import c from './Message.module.css'
 
-function Message() {
+type MessageDataType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+export function Message(props: MessageDataType) {
     return (
-        <div>
-
+        <div className={c.message}>
+            <div className={c.ava}>
+                <img src={props.avatar} />
+            </div>
+            <div className={c.textBox}>
+                <div className={c.name}>
+                    {props.name}
+                </div>
+                <div className={c.text}>
+                    {props.message}
+                </div>
+                <div className={c.time}>
+                    {props.time}
+                </div>
+            </div>
         </div>
     );
 }
-
-export default Message;
